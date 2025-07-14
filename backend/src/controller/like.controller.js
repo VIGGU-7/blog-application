6059ed3c4af249ed41462ebb7a26e3a5235ec9ab
VIGGU <vigneshpatlavath@gmail.com>
2025-07-14 10:52:59 +0530

    fixed issue in like controller

--- backend/src/controller/like.controller.js
+++ backend/src/controller/like.controller.js
@@ -1,41 +1,45 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import { like } from "../models/likes.model.js";
 
-export const likeOrDislike=async(req,res)=>{
-    const blogId=req.params.blogId;
-    const userId=req.user.id
-    try {
-        if(!blogId){
-            res.status(400).json({
-                message:"No blogid has been specified"
-            })
-        }
-        if(!mongoose.Types.ObjectId.isValid(blogId)){
-            res.status(400).json({
-                message:"Invalid blog Id"
-            })
-        }
-        const likeExists=await like.findOne({blogId:blogId,likedBy:userId})
-        if(likeExists){
-            await like.findOneAndDelete({blogId:blogId,likedBy:userId})
-            res.status(200).json({
-                message:"disliked"
-            })
-        }
-        const newLike=new like({
-            blogId:blogId,
-            likedBy:userId
-        })
-        await newLike.save()
-        res.status(200).json({
-            message:"liked"
-        })
-    } catch (error) {
-        console.log(error)
-          console.log("An error occured in likeOrDislike controller")
-        res.status(500).json({
-            message:"Internal server error"
-        }
-    )
+export const likeOrDislike = async (req, res) => {
+  const blogId = req.params.blogId;
+  const userId = req.user.id;
+
+  try {
+    if (!blogId) {
+      return res.status(400).json({
+        message: "No blog ID has been specified",
+      });
     }
-}
+
+    if (!mongoose.Types.ObjectId.isValid(blogId)) {
+      return res.status(400).json({
+        message: "Invalid blog ID",
+      });
+    }
+
+    const likeExists = await like.findOne({ blogId: blogId, likedBy: userId });
+
+    if (likeExists) {
+      await like.findOneAndDelete({ blogId: blogId, likedBy: userId });
+      return res.status(200).json({
+        message: "Disliked",
+      });
+    } else {
+      const newLike = new like({
+        blogId: blogId,
+        likedBy: userId,
+      });
+      await newLike.save();
+      return res.status(200).json({
+        message: "Liked",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("An error occurred in likeOrDislike controller");
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
