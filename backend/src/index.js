ff3720ae4c47d7e13a6bf8f55d771b64308e7000
VIGGU <vigneshpatlavath@gmail.com>
2025-07-14 14:23:37 +0530

    added network in cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import 'dotenv/config'
 const PORT=process.env.port || 8080
 const app = express();
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','http://192.168.0.100:5173'],
     credentials:true
 }))
 app.use(express.json({limit:'50mb'})); 
